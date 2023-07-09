1542f513989722ac243ed024b215b46fead3990a
shenderson62 <shendersongt26@gmail.com>
2023-07-09 00:53:23 -0400

    removed lighthouse pic and updated background color

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './Screen.css';
-import lighthouseBackground from '../../assets/images/lighthouse-background.png';
 
 function Screen() {
   return (
     <div className="screen-container">
-        <img className="image-overlay"src={lighthouseBackground} alt="lighthouse background"/>
     </div>
   );
 }
