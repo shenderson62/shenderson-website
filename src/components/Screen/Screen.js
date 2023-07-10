a3ae2a479e84e904732718374d4c87dab482cdf1
shenderson62 <shendersongt26@gmail.com>
2023-07-10 12:56:02 -0400

    navigate to route upon tab click

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Screen.css';
 
-function Screen({ children }) {
+const tabs = [
+  { id: 'about', label: 'About' },
+  { id: 'experience', label: 'Experience' },
+  { id: 'resume', label: 'Resume' },
+  { id: 'contact', label: 'Contact' },
+];
+
+function Screen({ children, activeTab: initialActiveTab }) {
+  const [activeTab, setActiveTab] = useState(initialActiveTab);
+  const location = useLocation();
+
+  React.useEffect(() => {
+    // Update the active tab based on the current route
+    const currentTab = tabs.find((tab) =>
+      location.pathname.toLowerCase().startsWith(`/${tab.id}`)
+    );
+    if (currentTab) {
+      setActiveTab(currentTab.id);
+    }
+  }, [location.pathname]);
+
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+
   return (
     <div className="screen-container">
       <div className="browser">
         <div className="title-bar">
           <div className="tabs">
-            <div className="tab active">About</div>
-            <div className="tab">Experience</div>
-            <div className="tab">Resume</div>
-            <div className="tab">Contact</div>
+            {tabs.map((tab) => (
+              <Link
+                key={tab.id}
+                to={`/${tab.id.toLowerCase()}`}
+                className={`tab ${activeTab === tab.id ? 'active' : ''}`}
+                onClick={() => handleTabClick(tab.id)}
+              >
+                {tab.label}
+              </Link>
+            ))}
           </div>
           <div className="buttons">
             <div className="minimize-button" />
             <div className="close-button" />
           </div>
         </div>
-        <div className="content">
-          {children}
-        </div>
+        <div className="content">{children}</div>
       </div>
     </div>
   );
