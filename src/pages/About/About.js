d630203268217854a0dd305fc08f6db1ce1cb8a9
shenderson62 <shendersongt26@gmail.com>
2023-07-11 15:14:13 -0400

    added basic about page styling

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Screen from '../../components/Screen/Screen';
 import Desktop from '../../components/Desktop/Desktop';
+import './About.css';
 
 function About() {
   return (
     <Desktop className="desktop">
-        <Screen className="screen">
-          <h1>About</h1>
-        </Screen>
+      <Screen className="screen">
+        <div className="top-section">
+          {/* Content for the top section */}
+        </div>
+        <div className="bottom-sections">
+          <div className="bottom-section left">
+            {/* Content for the first bottom section */}
+          </div>
+          <div className="bottom-section right">
+            {/* Content for the second bottom section */}
+          </div>
+        </div>
+      </Screen>
     </Desktop>
   );
 }
