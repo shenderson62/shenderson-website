b5aa0e0216440cf84efbf460e19b2e4b0442c181
shenderson62 <shendersongt26@gmail.com>
2023-07-11 21:20:51 -0400

    add some text to the about me page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,15 +8,22 @@ function About() {
     <Desktop className="desktop">
       <Screen className="screen">
         <div className="top-section">
-          {/* Content for the top section */}
+          <p>Hi, I'm Stephanie.</p>
         </div>
-        <div className="bottom-sections">
-          <div className="bottom-section left">
-            {/* Content for the first bottom section */}
-          </div>
-          <div className="bottom-section right">
-            {/* Content for the second bottom section */}
-          </div>
+        <div className="bottom-section left">
+          <p className="bottom-text">
+            I'm a graduate student at Georgia Tech, pursuing an MS in computer science with a specialization in machine learning.
+            With a deep passion for AI advancements and software development, I strive to stay at the forefront of these rapidly evolving fields.
+            <br /> <br />
+            My professional experience includes internships at companies such as Microsoft and Slalom Build, where I honed my skills
+            as a software engineer. These opportunities have not only shaped my expertise but have also instilled in me a drive to make
+            a tangible impact in the industry.
+            <br /> <br />
+            In addition to internship experience, I have had the privilege of serving as a teaching assistant at Georgia Tech for the
+            past two years. This experience has allowed me to collaborate closely with students and professors on challenging topics
+            in computer science, fostering a passion for knowledge sharing and academic growth. Together, we navigate through 
+            intricate concepts, further fueling my enthusiasm for the field.
+          </p>
         </div>
       </Screen>
     </Desktop>
